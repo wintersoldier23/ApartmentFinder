6458d5fad0ff911e7fd832015d395de36d46563b
Gasim Mammadov <Qasimbaku@hotmail.com>
2020-05-04 15:11:22 +0300

    initial commit

---
+++ Apartment Finder/Apartment Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apartment Finder
+//
+//  Created by Gasim Mammadov on 5/4/20.
+//  Copyright © 2020 Gasim Mammadov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
